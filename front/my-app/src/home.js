7f55a5ec50965597c6ed99dec9ceacf0358c4359
clue31415 <clue31415@gmail.com>
2025-05-30 21:32:46 +0900

    Update home.js

--- front/my-app/src/home.js
+++ front/my-app/src/home.js
@@ -10,7 +10,7 @@ export default function Home() {
     content:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
   }])
   const makedbreadable = async() => {
-    fetch("http://localhost:8080/api/users/read",{method: "GET"})
+    fetch("http://172.30.1.44:8080//api/users/read",{method: "GET"})
     .then(res => res.json())
     .then(data => {
 const _inputData = data.map((rowData)=> ({
