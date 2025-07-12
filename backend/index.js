2c767798b3098600fc1d7e49ca141034c4d71e59
clue31415 <clue31415@gmail.com>
2025-07-12 21:20:28 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -283,6 +283,11 @@ io.on('connection', (socket) => {
     }
   });
 
+  socket.on('ready', () => {
+    console.log('🟢 Both participants joined. You can receive an offer.');
+    createPeerConnection(false);  // 이거!
+  });
+    
   socket.on('offer', ({ roomId, sdp, type }) => {
     socket.to(roomId).emit('offer', { sdp, type });
     console.log(`📡 Offer sent in room ${roomId}`);
