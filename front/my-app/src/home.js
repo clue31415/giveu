bbc2f99506121bf677c92c2bd1980fb4a7db44e3
clue31415 <clue31415@gmail.com>
2025-07-12 20:47:52 +0900

    Update home.js

--- front/my-app/src/home.js
+++ front/my-app/src/home.js
@@ -10,7 +10,7 @@ export default function Home() {
     content:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
   }])
   const makedbreadable = async() => {
-    fetch("http://okpogo.servehttp.com/api/users/read",{method: "GET"})
+    fetch("https://okpogo.servehttp.com/api/users/read",{method: "GET"})
     .then(res => res.json())
     .then(data => {
 const _inputData = data.map((rowData)=> ({
