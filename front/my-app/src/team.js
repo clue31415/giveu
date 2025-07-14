2816fb8ea52be4226640afac942e1ceca8bf692f
clue31415 <clue31415@gmail.com>
2025-07-14 20:21:32 +0900

    Update team.js

--- front/my-app/src/team.js
+++ front/my-app/src/team.js
@@ -259,6 +259,7 @@ const createPeerConnection = async (isCaller) => {
   const change = (event) => {
     const value = event.target.value;
     setText(value); // 상태 업데이트
+    console.log(value,text,'v t');
     socket.emit('change-text', { roomId, text });
   };
 
